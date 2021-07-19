edfd4a8f1847f3c6f056c39b18d3e47e510f240a
Shuruhatik <72819916+Shuruhatik@users.noreply.github.com>
2021-07-19 14:16:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-const broadcast = require(`discord-broadcast`)//npm i discord-broadcast
+const broadcast = require(`discord-broadcast`)
 const Discord = require('discord.js');//npm i discord.js@dev
-const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES", 'GUILD_MESSAGES', 'GUILD_VOICE_STATES']});
-const discordvoice = require('@discordjs/voice');//npm i @discordjs/voice
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", 'GUILD_MESSAGES', 'GUILD_VOICE_STATES'] });
+const discordvoice = require('@discordjs/voice');
+const fetch = require("node-fetch")
 
 client.on(`ready`, async () => {
   console.log(`Bot Ready`)
   await broadcast.start({
-    channel: process.env['channel'], //The ID of the audio channel or stage channel that you want
-    youtube:  process.env['youtube'] || `https://www.youtube.com/watch?v=tkNS43oYxwY`, //The link to the live broadcast you want from YouTube
-    client:client, //Put the value of your client in discordjs
-    discordvoice:discordvoice //Put the value of your discordvoice in @discordjs/voice
+    channel: process.env['channel'], 
+    youtube: process.env['youtube'] || `https://www.youtube.com/watch?v=tkNS43oYxwY`,
+    client: client,discordjs
+    discordvoice: discordvoice 
   })
 })
 
+setInterval(async () => {
+  const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co/`
+  const response = await fetch(url, { headers: { 'User-Agent': 'by shuruhatik' } }).catch(err => {
+    console.log(`Failed to ping ${url}: ${err}`);
+  });
+  console.log(`\u001b[32mSuccessfully pinged ${url}\u001b[0m`);
+  status = response.status;
+}, 120000);
 
 client.login(process.env['token'])
