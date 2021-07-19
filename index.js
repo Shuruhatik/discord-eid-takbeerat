48fae34058f10e96f3b246168ad0d85b1c19485c
Shuruhatik <72819916+Shuruhatik@users.noreply.github.com>
2021-07-19 17:42:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const broadcast = require(`discord-broadcast`)
 const Discord = require('discord.js');//npm i discord.js@dev
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", 'GUILD_MESSAGES', 'GUILD_VOICE_STATES'] });
 const discordvoice = require('@discordjs/voice');
-const fetch = require("node-fetch")
+const Monitor = require('ping-monitor');
+const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co/`
+const myMonitor = new Monitor({website: url,title: process.env.REPL_SLUG,interval: 3 });
 
 client.on(`ready`, async () => {
-  console.log(`Bot Ready`)
+  console.log(`Bot Ready\nﻞﺼﺘﻣ ﺕﻮﺒﻟﺍ`)
   await broadcast.start({
     channel: process.env['channel'], 
     youtube: process.env['youtube'] || `https://www.youtube.com/watch?v=tkNS43oYxwY`,
@@ -13,14 +15,5 @@ client.on(`ready`, async () => {
     discordvoice: discordvoice 
   })
 })
-
-setInterval(async () => {
-  const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co/`
-  const response = await fetch(url, { headers: { 'User-Agent': 'by shuruhatik' } }).catch(err => {
-    console.log(`Failed to ping ${url}: ${err}`);
-  });
-  console.log(`\u001b[32mSuccessfully pinged ${url}\u001b[0m`);
-  status = response.status;
-}, 120000);
 
 client.login(process.env['token'])
